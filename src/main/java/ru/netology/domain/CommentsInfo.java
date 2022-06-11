1933d9cd2588a1cfea87eddf64355771037c19e0
Nadia Bash <pretty_belka_2018@bk.ru>
2022-06-11 07:44:19 +0500

    Initial commit

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private int canPost;
+    private boolean canOpen;
+    private boolean canClose;
+
+
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int fromId;
+    private int data;
+    private int text;
+
+}
