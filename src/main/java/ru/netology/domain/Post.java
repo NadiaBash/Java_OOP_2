abba1373e0e52a002837a2a387780897dc30eae1
Nadia Bash <pretty_belka_2018@bk.ru>
2022-06-11 08:07:13 +0500

    v2

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -4,6 +4,19 @@ public class Post {
     private int id;
     private int fromId;
     private int data;
-    private int text;
+    private String text;
+    private int type;
+    private  int createdBy;
+    private int friendsOnly;
+    private int replyPostId;
+    private int replyOwnerId;
+    private int ownerId;
+    private boolean canPin;
+    private boolean canEdit;
+    private boolean isFavorite;
+    private boolean markedAsAd;
+    private int signer_id;
+    private int postType;
+
 
 }
